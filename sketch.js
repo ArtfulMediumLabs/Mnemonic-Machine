cd445f34dfa78ce633f0bd5926306fde402f9d9f
Greg Cerveny <greg.cerveny@gmail.com>
2023-08-29 09:45:10 -0400

    menu images

--- sketch.js
+++ sketch.js
@@ -1,15 +1,28 @@
+function preload() {
+  dingImg = loadImage('img/ding.png');
+  toasterImg = loadImage('img/toaster.png');
+  waffleImg = loadImage('img/waffle.png');
+  leggoImg = loadImage('img/leggo.png');
+}
+
 function setup() {
-    createCanvas(1024, 768);
-  }
+    createCanvas(1920, 1080);
+}
+
+
+function draw() {
+  background('#FFD126');
+
+  let menuColor = color('#FFFFFF');
+  menuColor.setAlpha(76);
+  fill(menuColor);
+
+  noStroke();
   
-  function draw() {
-    background('#FFD126');
+  rect(0,0,308,1080);
 
-    let menuColor = color('#FFFFFF');
-    menuColor.setAlpha(76);
-    fill(menuColor);
-
-    noStroke();
-
-    rect(0,0,164,768);
-  }
+  image(dingImg, 73, 44);
+  image(toasterImg, 74, 305);
+  image(waffleImg, 71, 570);
+  image(leggoImg, 63, 857);
+}
