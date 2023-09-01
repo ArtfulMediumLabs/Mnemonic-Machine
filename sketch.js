2f813e754abfe2aa9bc4889f8c0df41a71cac861
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2023-08-31 23:02:55 -0400

    undoing controls image add

--- sketch.js
+++ sketch.js
@@ -110,7 +110,7 @@ function mousePressed() {
 
   if ( helpButton.inBounds(mouseX, mouseY) ) {
     let url = encodeURL();
-    modalTinyNoFooter.setContent(`<img src="img/controls.png">`);
+    modalTinyNoFooter.setContent(`<h2>Help!</h2>`);
     modalTinyNoFooter.open();
     return;
   }
