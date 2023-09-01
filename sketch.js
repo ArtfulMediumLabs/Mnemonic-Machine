3a6c0eb34229d0503e6398de1a08439370be12fc
Beacon Street Studios <143728792+Beacon-Street-Studios@users.noreply.github.com>
2023-08-31 23:14:00 -0400

    trying once more for controls image

--- sketch.js
+++ sketch.js
@@ -110,7 +110,7 @@ function mousePressed() {
 
   if ( helpButton.inBounds(mouseX, mouseY) ) {
     let url = encodeURL();
-    modalTinyNoFooter.setContent(`<img src="img/controls.png"`);
+    modalTinyNoFooter.setContent(`img/controls.png`);
     modalTinyNoFooter.open();
     return;
   }
